d10b6573bb0c63ec222f0c8eef2cf31c1cfdcf18
AbdullahNoori <51065314+AbdullahNoori@users.noreply.github.com>
2019-12-09 12:28:27 -0800

    added file with classes

---
+++ app.py
@@ -0,0 +1,34 @@
+class Person:
+    def did_survive_infection():
+
+
+class Virus:
+
+
+class Simulation:
+    def print_population():
+
+
+
+    def get_infected():
+
+
+
+    def simulation_should_continue():
+
+
+
+
+    def determine_survival():
+
+
+
+    def time_step():
+
+
+    def interaction():
+
+
+
+class FileWriter:
+
