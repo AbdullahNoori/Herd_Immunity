d39628e4bc16f213992a36322b0dfe7cba7160d9
AbdullahNoori <51065314+AbdullahNoori@users.noreply.github.com>
2019-12-12 22:12:09 -0800

    updated functions in simulaation py

--- app.py
+++ app.py
@@ -1,14 +1,47 @@
+import random, sys
+random.seed(42)
+from person import Person
+from logger import Logger
+from virus import Virus
+
+
 class Person:
-    def did_survive_infection():
+    def did_survive_infection(self, ):
 
 
 class Virus:
 
 
 class Simulation:
-    def print_population():
+
+    def __init__(self, initial_vaccinated, initial-infected, initial_healthy, virus, resultsfilename):
+        sel.virus = virus
+        self.initial_infected = initial_infected
+        self.initial_healthy = initial_vaccinated
+        
+        self.population = []
+
+        self.population_size = initial_infected + initial_healthy + initial_vaccinated
+        
+        self.total_dead = 0
+        self.total_vaccinated = initial-vaccinated
+        self.file_writer = FileWriter(resultsfilename)
 
 
+
+    def print_population(self):
+          '''Creates the population (a list of Person objects) consisting of initial infected people, initial healthy non-vaccinated people, and 
+        initial healthy vaccinated people. Adds them to the population list''' 
+        for i in range(self.initial_infected):
+            person = Person(False, virus)
+            self.population.append(person)
+
+        for i in range(self.intial_healthy):
+            person = Person(Flasem virus)
+            self. population.append(person)
+
+        for i in range(self.initial_vaccinated):
+            
 
     def get_infected():
 
@@ -22,12 +55,39 @@ class Simulation:
     def determine_survival():
 
 
-
+#time-step method
     def time_step():
+        infected_people = []
+        for person in self.population:
+            if person.infection != None:
+                infected_people.append(person_id)
 
-
+                for infected_person in infected:
+                    for encounter in range(100):
+                        random_person = None:
+                        while random person == None:
+                            randome_person = selected_person
+                        self.interaction(self.population[random.randint(0, len(self.population) -1)]
+                    for infected_person in infected_people:
+                        person = self.population[infected_person]
+                        survived = person.survived_infection(self.mortality_rate)
+                        self.logger.log_infection_survival(person, survived)
+                        if(not survived):
+                            self.died += 1 
+                        
+            self.infect_new_infected()
+               interaction. It assumes that only living people are passed in as parameters.
+        Args:
+            person1 (person): The initial infected person
+            random_person (person): The person that person1 interacts with.
+        '''
+                # Assert statements are included to make sure that only living people are passed
+                # in as params
+                assert person.is_alive == True
+                assert random_person.is_alive == True
+                        
     def interaction():
-
+        
 
 
 class FileWriter:
