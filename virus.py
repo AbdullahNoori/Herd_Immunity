a57c42ef182e13ec2cb1b7b6b5c3e0158f566cd2
AbdullahNoori <51065314+AbdullahNoori@users.noreply.github.com>
2019-12-13 06:23:17 -0800

    added virus py file

---
+++ virus.py
@@ -0,0 +1,9 @@
+class Virua:
+# Represents the virus that will bes used to infect people in the simulation'''
+
+    def __init__(self, name, reproduction_num, mortality_num):
+        self.name = name 
+        self.reproductive_num = reproduction_num
+        self.mmortality_num = mortality_num
+
+
